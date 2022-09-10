97651a8bf9841643620b4f4c951512913d253c72
Berdnikov Pavel <berdnikov_pavel_1991@mail.ru>
2022-09-10 13:11:59 +0400

    Initial commit

---
+++ Lesson 1/Task 1/ConsoleApplication1.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <fstream>
+
+
+int main(int argc, char** argv)
+{
+    std::string fileName = "in.txt";
+    std::ifstream fin(fileName);
+    std::ofstream fout("out.txt");
+
+    if(!fin.is_open())
+    {
+        std::cout << "Файл не найден!\n";
+        return 1;
+    }
+
+    int N = 0;
+    fin >> N;
+    int* arrN = new int[N];
+
+    for (int i = 0; i < N; ++i)
+    {
+        fin >> arrN[i];
+    }
+
+    int M = 0;
+    fin >> M;
+    int* arrM = new int[M];
+
+    for (int i = 0; i < M; ++i)
+    {
+        fin >> arrM[i];
+    }
+
+    fout << M << std::endl;
+    fout << arrM[M - 1] << " ";
+    for (int i = 0; i < M - 1; ++i)
+    {
+        fout << arrM[i] << " ";
+    }
+    fout << std::endl;
+
+    fout << N << std::endl;
+    for (int i = 1; i < N; ++i)
+    {
+        fout << arrN[i] << " ";
+    }
+    fout << arrN[0];
+
+    fin.close();
+    fout.close();
+    delete[]arrN;
+    arrN = nullptr;
+    delete[]arrM;
+    arrM = nullptr;
+    return 0;
+}
