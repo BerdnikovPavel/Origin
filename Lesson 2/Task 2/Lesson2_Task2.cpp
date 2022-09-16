db37855c02297acb64c1441c98d96239d64f4dda
Berdnikov Pavel <berdnikov_pavel_1991@mail.ru>
2022-09-16 17:03:27 +0400

    Lesson2 Task2&3

---
+++ Lesson 2/Task 2/Lesson2_Task2.cpp
@@ -0,0 +1,36 @@
+﻿
+#include <iostream>
+#include <locale.h>
+
+struct BankAccount
+{
+    long m_accountNumber = 0;
+    std::string m_ownersName;
+    double m_accountBalance = 0;
+};
+
+void newBalance(BankAccount &p, double newp)
+{
+    p.m_accountBalance = newp;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    system("chcp 1251");
+
+    BankAccount p;
+    double newp = 0;
+
+    std::cout << "Введите номер счёта: ";
+    std::cin >> p.m_accountNumber;
+    std::cout << "Введите имя владельца: ";
+    std::cin >> p.m_ownersName;
+    std::cout << "Введите баланс: ";
+    std::cin >> p.m_accountBalance;
+    std::cout << "Введите новый баланс: ";
+    std::cin >> newp;
+    newBalance(p, newp);
+    std::cout << "Ваш счёт: " << p.m_ownersName << ", " << p.m_accountNumber << ", " << p.m_accountBalance << std::endl;
+    return 0;
+}
---
+++ Lesson 2/Task 3/Lesson2_Task3.cpp
@@ -0,0 +1,36 @@
+﻿
+#include <iostream>
+
+struct Address
+{
+    std::string m_country;
+    std::string m_city;
+    std::string m_street;
+    int m_houseNumber;
+    int m_apartmentNumber;
+    int m_index;
+};
+
+void printAddress(Address p)
+{
+    std::cout << "Страна: " << p.m_country << std::endl
+            << "Город: " << p.m_city << std::endl
+            << "Улица: " << p.m_street << std::endl
+            << "Номер дома: " << p.m_houseNumber << std::endl
+            << "Номер квартиры: " << p.m_apartmentNumber << std::endl
+            << "Индекс: " << p.m_index << std::endl << std::endl;
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    Address one{ "Россия", "Москва", "Арбат", 12, 8, 123456 }, 
+            two{ "Россия", "Ижевск", "Пушкинская", 59, 143, 953769 };
+
+    printAddress(one);
+    printAddress(two);
+
+    return 0;
+}
