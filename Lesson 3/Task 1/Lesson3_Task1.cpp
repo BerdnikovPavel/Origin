3afcc905f9338654263d9ecbc28d8632c813322f
Berdnikov Pavel <berdnikov_pavel_1991@mail.ru>
2022-09-20 16:59:07 +0400

    Task1 fixed

--- Lesson 3/Task 1/Lesson3_Task1.cpp
+++ Lesson 3/Task 1/Lesson3_Task1.cpp
@@ -3,10 +3,10 @@
 
 class Calculator
 {
-public:
+private:
     double num1;
     double num2;
-
+public:
     double add()
     {
         return num1 + num2;
@@ -71,19 +71,21 @@ int main()
     setlocale(LC_ALL, "Russian");
 
     Calculator calc;
+    double num1 = 0;
+    double num2 = 0;
     for (; ; )
     {
         do
         {
             std::cout << "Введите num1: ";
-            std::cin >> calc.num1;
-        } while (calc.set_num1(calc.num1) == false);
+            std::cin >> num1;
+        } while (calc.set_num1(num1) == false);
 
         do
         {
             std::cout << "Введите num2: ";
-            std::cin >> calc.num2;
-        } while (calc.set_num2(calc.num2) == false);
+            std::cin >> num2;
+        } while (calc.set_num2(num2) == false);
 
         std::cout << "num1 + num2 = " << calc.add() << std::endl;
         std::cout << "num1 - num2 = " << calc.subtract_1_2() << std::endl;
@@ -92,6 +94,5 @@ int main()
         std::cout << "num1 / num2 = " << calc.divide_1_2() << std::endl;
         std::cout << "num2 / num1 = " << calc.divide_2_1() << std::endl;
     }
-
     return 0;
 }
