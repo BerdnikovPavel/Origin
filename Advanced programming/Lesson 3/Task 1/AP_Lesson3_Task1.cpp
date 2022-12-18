06889469cea2f6d2267ddb6ce0857b39a5fc24e4
Berdnikov Pavel <berdnikov_pavel_1991@mail.ru>
2022-12-18 16:28:23 +0400

    Advanced programming: Lesson 3 Task1 & Task2

---
+++ Advanced programming/Lesson 3/Task 1/AP_Lesson3_Task1.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include <algorithm>
+
+class smart_array 
+{
+private:
+    int actual_size;
+	int logical_size;
+	int* arr;
+public:
+	smart_array(int actual_size)
+	{
+		this->actual_size = actual_size;
+		this->arr = new int[actual_size];
+		this->logical_size = 0;
+	}
+
+	int add_element(int element)
+	{
+		if (logical_size == actual_size)
+		{
+			actual_size = actual_size * 2;
+			int* new_arr = new int[actual_size];
+			new_arr[logical_size] = element;
+			for (int i = 0; i < logical_size; ++i)
+			{
+				new_arr[i] = arr[i];
+			}
+			++logical_size;
+			delete[]arr;
+			arr = new_arr;
+			return *new_arr;
+		}
+		else
+		{
+			arr[logical_size] = element;
+			++logical_size;
+			return *arr;
+		}
+	}
+
+	int get_element(int index)
+	{
+		if (index < 0 || index > logical_size - 1) throw std::exception("Неверный индекс!");
+		return arr[index];
+	}
+	~smart_array()
+	{
+		delete[]arr;
+	}
+};
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	try {
+		smart_array arr(5);
+		arr.add_element(1);
+		arr.add_element(4);
+		arr.add_element(155);
+		arr.add_element(14);
+		arr.add_element(15);
+		std::cout << arr.get_element(1) << std::endl;
+		std::cout << arr.get_element(4) << std::endl;
+		arr.add_element(20);
+		arr.add_element(30);
+		std::cout << arr.get_element(5) << std::endl;
+		std::cout << arr.get_element(8) << std::endl;
+	}
+	catch (const std::exception& ex) {
+		std::cout << ex.what() << std::endl;
+	}
+	return 0;
+}
+
---
+++ Advanced programming/Lesson 3/Task 2/AP_Lesson3_Task2.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <memory>
+
+class smart_array
+{
+private:
+	int actual_size;
+	int logical_size;
+	int* arr;
+public:
+	smart_array& operator = (const smart_array& other)
+	{
+		for (int i = 0; i < logical_size; ++i)
+		{
+			arr[i] = other.arr[i];
+		}
+		delete[]other.arr;
+		return *this;
+	}
+	smart_array(int actual_size)
+	{
+		this->actual_size = actual_size;
+		this->arr = new int[actual_size];
+		this->logical_size = 0;
+	}
+
+	int add_element(int element)
+	{
+		if (logical_size == actual_size)
+		{
+			actual_size = actual_size * 2;
+			int* new_arr = new int[actual_size];
+			new_arr[logical_size] = element;
+			for (int i = 0; i < logical_size; ++i)
+			{
+				new_arr[i] = arr[i];
+			}
+			++logical_size;
+			delete[]arr;
+			arr = new_arr;
+			return *new_arr;
+		}
+		else
+		{
+			arr[logical_size] = element;
+			++logical_size;
+			return *arr;
+		}
+	}
+
+	int get_element(int index)
+	{
+		if (index < 0 || index > logical_size - 1) throw std::exception("Неверный индекс!");
+		return arr[index];
+	}
+	~smart_array()
+	{}
+};
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	try {
+		smart_array arr(5);
+		arr.add_element(1);
+		arr.add_element(4);
+		arr.add_element(155);
+
+		smart_array new_array(2);
+		new_array.add_element(44);
+		new_array.add_element(34);
+
+		arr = new_array;
+	}
+	catch (const std::exception& ex) {
+		std::cout << ex.what() << std::endl;
+	}
+	return 0;
+}
