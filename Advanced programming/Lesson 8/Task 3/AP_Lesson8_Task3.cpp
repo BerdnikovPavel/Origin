c9147b11a04349c68d77cceec5d5c3fc102b1243
Berdnikov Pavel <berdnikov_pavel_1991@mail.ru>
2023-01-19 16:50:48 +0400

    Advanced programming: Lesson 8, Task 1, 2 & 3

---
+++ Advanced programming/Lesson 8/Task 3/AP_Lesson8_Task3.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+template <class T>
+class My_unique_ptr
+{
+private:
+    T* ptr;
+public:
+    My_unique_ptr() : ptr(nullptr) {}
+
+    ~My_unique_ptr() { delete ptr; }
+
+    My_unique_ptr(const My_unique_ptr& value) = delete;
+
+    My_unique_ptr& operator=(const My_unique_ptr& value) = delete;
+
+    T& operator* () { return *ptr; }
+
+    T* release()
+    {
+        T* raw_ptr = ptr;
+        ptr = nullptr;
+        return raw_ptr;
+    }
+};
+
+int main()
+{
+    return 0;
+}
