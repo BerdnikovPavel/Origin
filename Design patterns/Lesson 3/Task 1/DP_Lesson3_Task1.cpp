b3a9b465c01a164eef9246604f5b88c88c9d3723
Berdnikov Pavel <berdnikov_pavel_1991@mail.ru>
2023-05-20 17:34:46 +0400

    DP: Lesson 3 Task 1 & 2

---
+++ Design patterns/Lesson 3/Task 1/DP_Lesson3_Task1.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <fstream>
+
+class LogCommand {
+public:
+    virtual ~LogCommand() = default;
+    virtual void print(const std::string& message) = 0;
+};
+
+class PrintToConsole : public LogCommand {
+public:
+    PrintToConsole() {}
+    void print(const std::string& message) override {
+        std::cout << message << std::endl;
+    }
+};
+
+class PrintToFile : public LogCommand {
+public:
+    PrintToFile(){}
+    void print(const std::string& message) override {
+        std::ofstream file("file.txt");
+        if (file.is_open())
+        {
+            file << message << std::endl;
+        }
+        else
+        {
+            std::cout << "Error";
+        }
+        file.close();
+    }
+};
+
+void print(LogCommand& x, std::string message) {
+    x.print(message);
+}
+
+int main()
+{
+    PrintToConsole* pts = new PrintToConsole;
+    print(*pts, "Print to console");
+    PrintToFile* ptf = new PrintToFile;
+    print(*ptf, "Print to file");
+    return 0;
+}
---
+++ Design patterns/Lesson 3/Task 2/DP_Lesson3_Task2.cpp
@@ -0,0 +1,92 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+
+class Observer {
+public:
+    virtual void onWarning(const std::string& message) {}
+    virtual void onError(const std::string& message) {}
+    virtual void onFatalError(const std::string& message) {}
+};
+
+class Subject {
+public:
+    void AddObserver(Observer* observer) {
+        observers_.push_back(observer);
+    }
+
+    void RemoveObserver(Observer* observer) {
+        auto it = std::remove(observers_.begin(), observers_.end(), observer);
+        observers_.erase(it, observers_.end());
+    }
+
+    void warning(const std::string& message) const {
+        for (auto observer : observers_) {
+            observer->onWarning(message);
+        }
+    }
+
+    void error(const std::string& message) const {
+        for (auto observer : observers_) {
+            observer->onError(message);
+        }
+    }
+
+    void fatalError(const std::string& message) const {
+        for (auto observer : observers_) {
+            observer->onFatalError(message);
+        }
+    }
+
+private:
+    std::vector<Observer*> observers_;
+};
+
+
+
+class ObserverW : public Observer {
+public:
+    void onWarning(const std::string& message) override {
+        std::cout << message << std::endl;
+    }
+};
+
+class ObserverE : public Observer {
+public:
+    void onError(const std::string& message) override {
+        std::ofstream file("file.txt");
+        if (file.is_open())
+        {
+            file << message << std::endl;
+        }
+        file.close();
+    }
+};
+
+class ObserverFE : public Observer {
+public:
+    void onFatalError(const std::string& message) override {
+        std::cout << message << std::endl;
+        std::ofstream file("file.txt");
+        if (file.is_open())
+        {
+            file << message << std::endl;
+        }
+        file.close();
+    }
+};
+
+int main()
+{
+    Subject* sub = new Subject;
+    ObserverW* ow = new ObserverW;
+    ObserverE* oe = new ObserverE;
+    ObserverFE* ofe = new ObserverFE;
+    sub->AddObserver(ow);
+    sub->AddObserver(oe);
+    sub->AddObserver(ofe);
+    sub->warning("Warning");
+    sub->error("Error");
+    sub->fatalError("FatalError");
+    return 0;
+}
