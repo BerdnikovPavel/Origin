8fed56b135994b2e42daa024fbaa5e03d171032d
Berdnikov Pavel <berdnikov_pavel_1991@mail.ru>
2023-05-21 19:28:56 +0400

    DP: Lesson 4 Task 1 & 2

---
+++ Design patterns/Lesson 4/Task 1/DP_Lesson4_Task1.cpp
@@ -0,0 +1,89 @@
+﻿#include <fstream>
+
+class Printable
+{
+public:
+    virtual ~Printable() = default;
+
+    virtual std::string printAsHTML() const = 0;
+    virtual std::string printAsText() const = 0;
+    virtual std::string printAsJSON() const = 0;
+};
+
+/*В классе который написан выше нарушен принцип разделения интрефейса. Получается он перегружен методами которые не использует. 
+Лучше было написать один абстрактный метод, а в классах наследниках специализировать его под конкретный формат*/
+
+class Data : public Printable
+{
+public:
+    enum class Format
+    {
+        kText,
+        kHTML,
+        kJSON
+    };
+
+    Data(std::string data, Format format)
+        : data_(std::move(data)), format_(format) {}
+
+    std::string printAsHTML() const override
+    {
+        if (format_ != Format::kHTML) {
+            throw std::runtime_error("Invalid format!");
+        }
+        return "<html>" + data_ + "<html/>";
+    }
+    std::string printAsText() const override
+    {
+        if (format_ != Format::kText) {
+            throw std::runtime_error("Invalid format!");
+        }
+        return data_;
+    }
+    std::string printAsJSON() const override
+    {
+        if (format_ != Format::kJSON) {
+            throw std::runtime_error("Invalid format!");
+        }
+        return "{ \"data\": \"" + data_ + "\"}";
+    }
+
+    /*В переопределенных выше функциях нарушен принцип подстановки Лисков. В базовом классе видно, что функция должна возвращать строку, 
+    однако в классе наследнике, переопределенная функция меняет свое повидение, теперь она может выбрасывать исключение, 
+    что противоречит принципу подстановки Лисков*/
+
+private:
+    std::string data_;
+    Format format_;
+};
+
+void saveTo(std::ofstream& file, const Printable& printable, Data::Format format)
+{
+    switch (format)
+    {
+    case Data::Format::kText:
+        file << printable.printAsText();
+        break;
+    case Data::Format::kJSON:
+        file << printable.printAsJSON();
+        break;
+    case Data::Format::kHTML:
+        file << printable.printAsHTML();
+        break;
+    }
+}
+/*В функции которая написана выше нарушен принцип откыртости/закрытости.Если в дальнейшем потребуется
+какой то новый формат для работы, придется дописывать новые кейсы.
+*/
+
+void saveToAsHTML(std::ofstream& file, const Printable& printable) {
+    saveTo(file, printable, Data::Format::kHTML);
+}
+
+void saveToAsJSON(std::ofstream& file, const Printable& printable) {
+    saveTo(file, printable, Data::Format::kJSON);
+}
+
+void saveToAsText(std::ofstream& file, const Printable& printable) {
+    saveTo(file, printable, Data::Format::kText);
+}
---
+++ Design patterns/Lesson 4/Task 2/DP_Lesson4_Task2.cpp
@@ -0,0 +1,62 @@
+﻿#include <fstream>
+
+class Printable
+{
+public:
+    virtual ~Printable() = default;
+
+    virtual std::string printAs() const = 0;
+};
+
+class PrintableText : public Printable
+{
+public:
+    PrintableText(std::string data) : data_(std::move(data)) {}
+    std::string printAs() const override
+    {
+        return data_;
+    }
+private:
+    std::string data_;
+};
+
+class PrintableHTML : public Printable
+{
+public:
+    PrintableHTML(std::string data) : data_(std::move(data)) {}
+    std::string printAs() const override
+    {
+        return "<html>" + data_ + "<html/>";
+    }
+private:
+    std::string data_;
+};
+
+class PrintableJSON : public Printable
+{
+public:
+    PrintableJSON(std::string data) : data_(std::move(data)) {}
+    std::string printAs() const override
+    {
+        return "{ \"data\": \"" + data_ + "\"}";
+    }
+private:
+    std::string data_;
+};
+
+void saveTo(std::ofstream& file, const Printable& printable)
+{
+        file << printable.printAs();
+}
+
+void saveToAsHTML(std::ofstream& file, const PrintableHTML& printable) {
+    saveTo(file, printable);
+}
+
+void saveToAsJSON(std::ofstream& file, const PrintableJSON& printable) {
+    saveTo(file, printable);
+}
+
+void saveToAsText(std::ofstream& file, const PrintableText& printable) {
+    saveTo(file, printable);
+}
